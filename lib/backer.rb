cc0d5fd0f48b7fa6332ff1e2baa801d6406ab047
Trevis McGill <trevismcgill25@gmail.com>
2020-08-11 03:29:18 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,11 +4,11 @@ attr_reader :backed_projects
 
 def initialize(name)
   @name = name
-  @backed_projects = []
+  @@backed_projects = []
 end
 
 def back_project(project)
-@backed_projects << project
+@@backed_projects << project
 Project.backers << self
 end
 
